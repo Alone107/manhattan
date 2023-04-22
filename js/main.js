7a255bf6389a31e3dcc4e5a12e948055562ee991
Alone107 <123736524+Alone107@users.noreply.github.com>
2023-04-22 22:14:29 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 
 $('.kitchen__slider').slick({
 });
+
+$('.burger,.overlay').on('click', function (e) {
+  e.preventDefault()
+  $('.header__nav-list').toggleClass('header__nav-list--open')
+  $('.overlay').toggleClass('overlay--show')
+  $('.burger').toggleClass('active')
+  $('body').toggleClass('scroll')
+})
+
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
+  
+  
+
+    
